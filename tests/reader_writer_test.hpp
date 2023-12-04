0548c58ee680cd481de3a23b894320c1950147a8
Luukas Karihtala <luukas.karihtala@gmail.com>
2023-12-04 19:30:28 +0200

    Fix tests.

--- tests/reader_writer_test.hpp
+++ tests/reader_writer_test.hpp
@@ -4,32 +4,32 @@
 
 #include "ReaderWriter.hpp"
 
-inline void test_ReaderWriter_readFile() {
+inline void test_ReaderWriter_read_file() {
     ReaderWriter rw;
 
-    auto level = rw.readFile("tests/test_files/test_level.bblvl");
+    auto level = rw.read_file("tests/test_files/test_level.bblvl");
 
     verify(level.has_value(), "Failed to load level.");
-    verify(level->getEntities().size() == 14, "Entity counts.");
-    verify(level->getBirds().size() == 6, "Bird counts.");
+    verify(level->get_entities().size() == 14, "Entity counts.");
+    verify(level->get_birds().size() == 6, "Bird counts.");
 }
 
-inline void test_ReaderWriter_writeFile() {
+inline void test_ReaderWriter_write_file() {
     ReaderWriter rw;
 
     std::string test_path = "tests/test_files/test_write.bblvl";
 
-    Level lvl({}, {}, "", "", {}, "LEVEL", {}, test_path);
+    Level lvl({}, {}, "", "LEVEL", {}, test_path);
 
-    rw.writeFile(lvl, test_path);
+    rw.write_file(lvl, test_path);
 
-    auto read_lvl = rw.readFile(test_path);
+    auto read_lvl = rw.read_file(test_path);
 
     verify(read_lvl.has_value(), "Failed to load level.");
-    verify(lvl.getName() == read_lvl->getName(), "Name match.");
+    verify(lvl.get_name() == read_lvl->get_name(), "Name match.");
 }
 
 inline void tests_reader_writer() {
-    test_ReaderWriter_readFile();
-    test_ReaderWriter_writeFile();
+    test_ReaderWriter_read_file();
+    test_ReaderWriter_write_file();
 }
